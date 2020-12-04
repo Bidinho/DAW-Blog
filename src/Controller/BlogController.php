5235e53e03e6d5b5274fbc5ab4c40813ab602978
Ricardo Correia <rjcorreia12@gmail.com>
2020-12-04 16:46:30 +0000

    sample added

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -9,12 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
         ]);
+    }
+
+    /**
+     * @Route("/sample", name="sample")
+     */
+    public function sample()
+    {
+        return new Response('Sample');
     }
 }
