0152103bff929bcb1d491ab7f08a820acccd9355
Ricardo Correia <rjcorreia12@gmail.com>
2020-12-05 16:39:44 +0000

    started to add templates

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,26 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/sample", name="home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('blog/index.html.twig', [
+        return $this->render('home/index.html.twig', [
             'controller_name' => 'BlogController',
         ]);
     }
 
     /**
-     * @Route("/", name="sample")
+     * @Route("/custom/{name?}", name="custom")
+     * @param Request $request
+     * @return Response
      */
-    public function sample()
+    public function custom(Request $request)
     {
-        return new Response('Sample');
+        $name = $request->get('name');
+        return $this->render('home/custom.html.twig',[
+            'name' => $name
+        ]);
     }
 }
